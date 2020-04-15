e1093a3a2e1eca7bbf18b50c85b25ce4bf25eddc
Jennie Robinson Faber <jennie@dmg.to>
2020-04-15 15:09:59 -0400

    Minor style fix

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -74,14 +74,18 @@
                 alt="Creative Commons Licence"
                 style="border-width:0"
                 src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png"/></a
-            ><br />This work is licensed under a
-            <a
-              class="text-white"
-              rel="license"
-              href="http://creativecommons.org/licenses/by-nc-sa/4.0/"
-              >Creative Commons Attribution-NonCommercial-ShareAlike 4.0
-              International License</a
-            >.
+            ><br />
+            <div>
+              This work is licensed under a
+              <a
+                class="text-white"
+                rel="license"
+                href="http://creativecommons.org/licenses/by-nc-sa/4.0/"
+                >Creative Commons Attribution-NonCommercial-ShareAlike 4.0
+                International License</a
+              >
+            </div>
+            .
           </div>
         </div>
       </div>
