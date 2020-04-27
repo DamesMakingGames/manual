c47b40f7ec173e67315b42f19615d4a7dad71640
Jennie Robinson Faber <jennie@dmg.to>
2020-04-27 17:54:15 -0400

    Add edit button

--- src/templates/Post.vue
+++ src/templates/Post.vue
@@ -1,37 +1,62 @@
 <template>
   <Layout>
     <div class="container-inner mx-auto my-16">
-      <h1 class="text-4xl font-bold leading-tight">{{ $page.post.title }}</h1>
-
-      <div class="flex my-8 text-sm">
-        <g-link
-          :to="tag.path"
-          v-for="tag in $page.post.tags"
-          :key="tag.id"
-          class="tag bg-gray-200 rounded-full px-4 py-2 mr-4 hover:bg-gray-400 no-underline uppercase"
-        >{{ tag.title }}</g-link>
-      </div>
+      <header class="border-b border-gray-200 mb-12">
+        <a
+          :href="
+            `https://github.com/DamesMakingGames/manual/blob/master/${
+              $page.post.fileInfo.directory
+            }/${$page.post.fileInfo.name}${$page.post.fileInfo.extension}`
+          "
+          class="btn btn-purple float-right"
+          >Edit</a
+        >
+        <h1 class="text-4xl font-bold leading-tight">{{ $page.post.title }}</h1>
+        <div class="flex my-8 text-sm">
+          <g-link
+            :to="tag.path"
+            v-for="tag in $page.post.tags"
+            :key="tag.id"
+            class="tag bg-gray-200 rounded-full px-4 py-2 mr-4 hover:bg-gray-400 no-underline uppercase"
+            >{{ tag.title }}</g-link
+          >
+        </div>
+      </header>
       <div class="markdown-body mb-8" v-html="$page.post.content" />
-      <div class="text-sm text-gray-600 mb-4">Last revised: {{ $page.post.date }}</div>
+      <div class="text-sm text-gray-600 mb-4">
+        Last revised: {{ $page.post.date }}
+      </div>
       <div class="mb-8">
-        <g-link to="/manual" class="font-bold uppercase">Back to Manual Index</g-link>
+        <g-link to="/manual" class="font-bold uppercase"
+          >Back to Manual Index</g-link
+        >
       </div>
     </div>
   </Layout>
 </template>
 
 <page-query>
-query Post ($path: String!) {
-  post: post (path: $path) {
+
+query Post($path: String!) {
+  post: post(path: $path) {
     title
-    date (format: "MMMM D, Y")
+    path
+    date(format: "MMMM D, Y")
     content
     tags {
       title
       path
     }
+    fileInfo {
+      directory
+      name
+      extension
+    }
+    timeToRead
   }
 }
+
+
 </page-query>
 <style lang="postcss" scoped>
 .tag {
@@ -42,11 +67,10 @@ query Post ($path: String!) {
 export default {
   metaInfo() {
     return {
-      title: this.$page.post.title
+      title: this.$page.post.title,
     };
-  }
+  },
 };
 </script>
 
 <style src="../css/github-markdown.css" />
-
