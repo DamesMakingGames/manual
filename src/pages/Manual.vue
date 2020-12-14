0b96d5831e732e7021788126a199d28d3883ad97
Jennie Robinson Faber <jennie@dmg.to>
2020-12-14 01:23:41 -0500

    Add table styles for clarity

--- src/pages/Manual.vue
+++ src/pages/Manual.vue
@@ -11,7 +11,9 @@
         </h2>
         <ul>
           <li v-for="post in tag.node.belongsTo.edges" :key="post.id" class>
-            <g-link :to="post.node.path" class="post-link">{{ post.node.title }}</g-link>
+            <g-link :to="post.node.path" class="post-link">{{
+              post.node.title
+            }}</g-link>
           </li>
         </ul>
       </div>
@@ -48,17 +50,56 @@ query Dato {
 .post-link {
   @apply text-gray-800 font-normal leading-normal;
 }
+
+table {
+  padding: 0;
+}
+
+table tr {
+  border-top: 1px solid #cccccc;
+  background-color: white;
+  margin: 0;
+  padding: 0;
+}
+
+table tr:nth-child(2n) {
+  background-color: #f8f8f8;
+}
+
+table tr th {
+  font-weight: bold;
+  border: 1px solid #cccccc;
+  text-align: left;
+  margin: 0;
+  padding: 6px 13px;
+}
+
+table tr td {
+  border: 1px solid #cccccc;
+  text-align: left;
+  margin: 0;
+  padding: 6px 13px;
+}
+
+table tr th :first-child,
+table tr td :first-child {
+  margin-top: 0;
+}
+
+table tr th :last-child,
+table tr td :last-child {
+  margin-bottom: 0;
+}
 </style>
 <script>
 import PaginationPosts from "../components/PaginationPosts";
 
 export default {
   metaInfo: {
-    title: "DMG Manual Index"
+    title: "DMG Manual Index",
   },
   components: {
-    PaginationPosts
-  }
+    PaginationPosts,
+  },
 };
 </script>
-
